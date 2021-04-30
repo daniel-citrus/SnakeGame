330d26f8b9fd9c952cd6057fb57e70221dfc381c
Daniel Calvo <dcalvo@accfb.org>
2021-04-30 13:09:03 -0700

    User Input
    Created a simple loop to receive user input. I still need to research for a good main game loop to implement.

--- snake_game/user_input.c
+++ snake_game/user_input.c
@@ -8,12 +8,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 #define ESC 27
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
+#define KEY_SPACE 32
 
 int main()
 {
@@ -33,7 +35,25 @@ int main()
 				// your logic goes here
 				printf("Up.\n");
 				break;
+            case KEY_DOWN:
+				// your logic goes here
+				printf("Down.\n");
+				break;
+            case KEY_LEFT:
+				// your logic goes here
+				printf("Left.\n");
+				break;
+            case KEY_RIGHT:
+				// your logic goes here
+				printf("Right.\n");
+				break;
+            case KEY_SPACE:
+				// your logic goes here
+				printf("Space.\n");
+				break;
         }
     }
-    while(ch != 27);
+    while(ch != KEY_SPACE);
+
+    printf("...\nTerminated.");
 }
